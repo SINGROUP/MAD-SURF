43ebdf09eec54b503709b769d9733dd6fcaf46d2
Joakim Jestilä <88662850+joakimsj@users.noreply.github.com>
2026-01-21 11:08:22 +0200

    Update preprocess_full_dataset.sh
    removing specific paths and settings to our environment

--- adsorbgmlip/mace_train/full_dataset/preprocess_full_dataset.sh
+++ adsorbgmlip/mace_train/full_dataset/preprocess_full_dataset.sh
@@ -13,14 +13,13 @@ echo $OMP_NUM_THREADS
 
 mkdir processed_data
 
-python /MAHTI_TYKKY_2Q2XasS/miniforge/envs/env1/lib/python3.10/site-packages/mace/cli/preprocess_data.py \
---train_file="/scratch/project_2012660/MACE_training/dataset_full/train_dataset_std.extxyz" \
+python PATH_TO_MACE_ENVIRONMENT/mace/cli/preprocess_data.py \
+--train_file="PATH_TO_TRAINING_SET/train_dataset_std.extxyz" \
 --valid_fraction=0.05 \
---test_file="/scratch/project_2012660/MACE_training/dataset_full/test_dataset_std.extxyz" \
+--test_file="PATH_TO_TEST_SET/test_dataset_std.extxyz" \
 --atomic_numbers="[1, 6, 7, 8, 16, 17, 29, 35, 47, 79]" \
 --r_max=5.0 \
 --h5_prefix="processed_data/" \
 --E0s='{1:-13.6053084484, 6:-1029.1044589780, 7:-1485.3138688893, 8:-2043.2261372665, 16:-10875.6934658087, 17:-12577.6050159480, 29:-45242.4130555924, 35:-71401.3775787682, 47:-146385.3119095800, 79:-535650.8971237560}' \
 --seed=123 \
-
 
